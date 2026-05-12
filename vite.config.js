b2f56d5b3de2671c40d08318ef3520b12afed6cf
kuldeeprana2012 <kuldeeprana2012@gmail.com>
2026-05-12 20:18:03 +0530

    delay updated

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      ignored: [
+        '**/node_modules/**',
+        '**/.git/**',
+        '**/dist/**',
+        '**/*.tmp',
+        '**/*.swp',
+        '**/*~',
+        '**/.DS_Store',
+      ],
+    },
+    hmr: {
+      overlay: false,
+    },
+  },
 })
